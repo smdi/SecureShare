690c490a455e6be9ad869af5afe75517bb8f1526
immucse <smdimran838@gmail.com>
2018-01-20 19:41:34 +0530

    Initial commit

---
+++ app/src/main/java/al_muntaqimcrescent2018/com/secureshare/SignUp.java
@@ -0,0 +1,14 @@
+package al_muntaqimcrescent2018.com.secureshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+    }
+}
